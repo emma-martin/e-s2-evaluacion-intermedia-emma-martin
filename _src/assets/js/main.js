a7ee4ba35fc139adbb0cd38d80ed27efd4181e7e
emma-martin <emmamartinalvarez@gmail.com>
2018-12-03 18:02:03 +0100

    Set main form and main Js function

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -1,12 +1,37 @@
 'use strict';
 
-console.log('>> Ready :)');
 
 const inputNumber = document.querySelector('.input');
 const submitBtn = document.querySelector('.btn-send');
+const triesCounter = document.querySelector('.meter-counter');
+const answersBox = document.querySelector('.info');
 
-function getRandomNumber(max) {
+
+
+
+
+
+function guessNumber(event){
+    //random function and a const for me ==>> randomNum
+    function getRandomNumber(max) {
     return Math.ceil(Math.random() * max);
+    }
+    const randomNum = getRandomNumber(100);
+    console.log(randomNum);
+    event.preventDefault();
+
+    if(inputNumber.innerHTML === randomNum){
+        answersBox.innerHTML = `You made it! The number is ${randomNum}`;
+    } 
+    else if (inputNumber.innerHTML > randomNum){
+        answersBox.innerHTML = `Too close but too high! The number is ${randomNum}`;
+    }
+    else if (inputNumber.innerHTML < randomNum){
+        answersBox.innerHTML = `Too close but too low! The number is ${randomNum}`;
+    }
+    else {
+        answersBox.innerHTML = `Better try another game`;
+    }
 }
 
-submitBtn.addEventListener('click', )
+submitBtn.addEventListener('click', guessNumber);
