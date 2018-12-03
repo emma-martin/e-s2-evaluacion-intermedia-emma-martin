1c6a1710da0ffef00673399d3c5ac7f24978b6cb
emma-martin <emmamartinalvarez@gmail.com>
2018-12-03 17:41:52 +0100

    Add main and fix js route

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -1,3 +1,12 @@
 'use strict';
 
 console.log('>> Ready :)');
+
+const inputNumber = document.querySelector('.input');
+const submitBtn = document.querySelector('.btn-send');
+
+function getRandomNumber(max) {
+    return Math.ceil(Math.random() * max);
+}
+
+submitBtn.addEventListener('click', )
