63c9c3814672a7398bed1651230fe0d42ecc701f
emma-martin <emmamartinalvarez@gmail.com>
2018-12-06 13:34:20 +0100

    Fix issues #3,4,5,6,8,9

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -1,52 +1,35 @@
 'use strict';
-
-
 const inputNumber = document.querySelector('.input');
 const submitBtn = document.querySelector('.btn-send');
 const triesCounter = document.querySelector('.meter-counter');
 const answersBox = document.querySelector('.info');
 
-
-
-
-
-
-function guessNumber(event){
-    //random function and a const for me ==>> randomNum
-    function getRandomNumber(max) {
+function getRandomNumber(max) {
     return Math.ceil(Math.random() * max);
     }
-    const randomNum = getRandomNumber(100);
+const randomNum = getRandomNumber(100);
     console.log(randomNum);
+let tries = 0; 
+function guessNumber(event){
     event.preventDefault();
-
-    let numberEntered = inputNumber.value;
-
+    let numberEntered = parseInt(inputNumber.value);
+    console.log(numberEntered);
+    tries ++;
+    triesCounter.innerHTML = `You've tried ${tries} times`;
     if(numberEntered === randomNum){
-        answersBox.innerHTML = `You made it! The number is ${randomNum}`;
+        answersBox.innerHTML = `You made it! The number is ${randomNum} and it only took you ${tries} attempts`;
     } 
     else if (numberEntered > randomNum){
-        answersBox.innerHTML = `Too close but too high! The number is ${randomNum}`;
+        answersBox.innerHTML = `Too close but too high!`;
     }
     else if (numberEntered < randomNum){
-        answersBox.innerHTML = `Too close but too low! The number is ${randomNum}`;
+        answersBox.innerHTML = `Too close but too low!`;
     }
     else {
         answersBox.innerHTML = `Better try another game`;
     }
-
-    HowManyTries();
 }
 
+
+
 submitBtn.addEventListener('click', guessNumber);
-
-// let acc = 0;
-// function HowManyTries (tries) {
-//     for (let i = 0; i < 30; i++) {
-//         acc += tries[i];
-//         triesCounter.innerHTML = acc;
-//     }
-// }
-
-
-//todo lo que traemos de html, aunque sea value, hay que parseInt
